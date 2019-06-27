3b7e1b741fab3015563d032e9338189e5d34ac7c
chauhuynh2608 <52212600+chauhuynh2608@users.noreply.github.com>
2019-06-27 20:06:48 +0700

    testhamcrest

--- src/test/java/com/chauhuynh/autoweb/steps/serenity/LearnAssertApplicationInfoWithHamcrest.java
+++ src/test/java/com/chauhuynh/autoweb/steps/serenity/LearnAssertApplicationInfoWithHamcrest.java
@@ -5,14 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 
 @RunWith(SerenityRunner.class)
-public class LearnAboutTheApplication {
-
+public class LearnAssertApplicationInfoWithHamcrest {
 	@Managed(driver = "chrome")
 	public WebDriver driver;
 
@@ -27,7 +32,18 @@ public class LearnAboutTheApplication {
 
 		String expectedPageTitle = "AngularJS • TodoMVC";
 
-		Assert.assertEquals(acutalPageTitle, expectedPageTitle);
+		assertThat(acutalPageTitle, is(expectedPageTitle));
+
+	}
+
+	@Test
+	public void checkFooter() {
+
+		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+		driver.get("http://todomvc.com/examples/angularjs/#/");
+		String footer = driver.findElement(By.xpath("//footer/p[2]")).getText();
+		assertThat(footer, containsString("Credits"));
+
 	}
 
 	@Test
@@ -40,27 +56,7 @@ public class LearnAboutTheApplication {
 		String heading = driver.findElement(By.tagName("h1")).getText();
 
 		String expectedheading = "todos";
-
-		Assert.assertEquals(heading, expectedheading);
+		assertThat(heading, is(expectedheading));
 	}
 
-	@Test
-	public void checkFooter() {
-
-		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
-		driver.get("http://todomvc.com/examples/angularjs/#/");
-		String footer = driver.findElement(By.xpath("//footer/p[contains(text(),'Credits')]")).getText();
-		String expectedfooter = "Credits: Christoph Burgdorf, Eric Bidelman, Jacob Mumm and Igor Minar";
-		Assert.assertEquals(footer, expectedfooter);
-
-	}
-
-	@Test
-	public void checkFooter1() {
-
-		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
-		driver.get("http://todomvc.com/examples/angularjs/#/");
-		String footer = driver.findElement(By.xpath("//footer/p[2]")).getText();
-		Assert.assertTrue(footer.contains("Credits"));
-	}
 }
