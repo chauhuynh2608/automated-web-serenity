499326026eece36e02a7a830944a8c47ad062d70
chauhuynh2608 <52212600+chauhuynh2608@users.noreply.github.com>
2019-07-03 19:27:49 +0700

    checkInputEnterText using assert for Junit

--- src/test/java/com/chauhuynh/autoweb/infor/BasePageWebDriver.java
+++ src/test/java/com/chauhuynh/autoweb/infor/BasePageWebDriver.java
@@ -1,34 +1,25 @@
 package com.chauhuynh.autoweb.infor;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 
 @RunWith(SerenityRunner.class)
 public class BasePageWebDriver {
+	
 	@Managed
 	public WebDriver driver;
-
-	public BasePageWebDriver() {
-		super();
-	}
-
-	@BeforeTest
-	public void setUp() {
-		driver = new ChromeDriver();
-	}
 
 	@Before
 	public void setup() {
 		driver.get("http://todomvc.com/examples/angularjs/#/");
 	}
 
-	@AfterTest
+	@After
 	public void afterTest() {
 		driver.quit();
 	}
--- src/test/java/com/chauhuynh/autoweb/infor/CheckInputEnterText.java
+++ src/test/java/com/chauhuynh/autoweb/infor/CheckInputEnterText.java
@@ -1,36 +1,23 @@
 package com.chauhuynh.autoweb.infor;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
+import org.openqa.selenium.WebElement;
 import net.serenitybdd.junit.runners.SerenityRunner;
-import net.thucydides.core.annotations.Managed;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SerenityRunner.class)
+public class CheckInputEnterText extends BasePageWebDriver {
 
-public class CheckInputEnterText{
-	@Managed
-	public WebDriver driver;
-	
-	@Before
-	public void setup() {
-		driver.get("http://todomvc.com/examples/angularjs/#/");
-	}
 	@Test
 	public void checkInputEnterText() {
-		//Actions action = new Actions(this.driver);	
-		WebElement inputEnterText = driver.findElement(By.xpath("//input[@ng-model='newTodo']"));
-		inputEnterText.sendKeys("I am the one");
-		inputEnterText.sendKeys(Keys.ENTER);
-		String expected  = inputEnterText.getText(); 
-		Assert.assertEquals(inputEnterText, expected);
+		WebElement inputEnterText = driver.findElement(By.cssSelector(".new-todo"));
+		inputEnterText.sendKeys("I am the one", Keys.ENTER);
+		WebElement inputafterEnterText = driver.findElement(By.xpath("//ul/li//label"));
+		String expected = inputafterEnterText.getText();
+		assertEquals("I am the one", expected);
 	}
 }
